fe07d442107c05def7f87041965b963efe596d28
Max <109047395+maxsteeel@users.noreply.github.com>
2025-12-06 10:23:45 -0400

    Update build-lld.sh

--- build-lld.sh
+++ build-lld.sh
@@ -12,7 +12,9 @@ while getopts a: flag; do
     arch="$OPTARG"
     case "${OPTARG}" in
       "arm") ARCH_CLANG="ARM" && TARGET_CLANG="arm-linux-gnueabi" && TARGET_GCC="arm-eabi" ;;
+      "armgnu") ARCH_CLANG="ARM" && TARGET_CLANG="arm-linux-gnueabi" && TARGET_GCC="arm-linux-gnueabi" ;;
       "arm64") ARCH_CLANG="AArch64" && TARGET_CLANG="aarch64-linux-gnu" && TARGET_GCC="aarch64-elf" ;;
+      "arm64gnu") ARCH_CLANG="AArch64" && TARGET_CLANG="aarch64-linux-gnu" && TARGET_GCC="aarch64-linux-gnu" ;;
       "x86") ARCH_CLANG="X86" && TARGET_CLANG="x86_64-linux-gnu" && TARGET_GCC="x86_64-elf" ;;
       *) echo "Invalid architecture passed: $OPTARG" && exit 1 ;;
     esac
